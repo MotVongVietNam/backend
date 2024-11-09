cea975ce8891cace79f9ca594b4dc70609185d42
Hung Hoang <22021170@vnu.edu.vn>
2024-11-09 11:36:36 +0700

    update dish entity

--- src/modules/dishes/entity/feedback.entity.ts
+++ src/modules/dishes/entity/feedback.entity.ts
@@ -1,14 +1,10 @@
 import { User } from 'src/modules/users/entities/user.entity';
 import { BaseEntity } from 'src/utils/entity/base-entity';
-import { Column, Entity, Index, ManyToOne } from 'typeorm';
+import { Column, Entity, ManyToOne } from 'typeorm';
 import { Dish } from './dish.entity';
 
 @Entity('dish_feedback')
 export class DishFeedback extends BaseEntity {
-  @Column({ nullable: false })
-  @Index()
-  dish_id: number;
-
   @Column(() => User)
   user: User;
 
