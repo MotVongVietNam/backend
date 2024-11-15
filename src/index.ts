a1f8ac6511b2dea80478b4f5670e8622d65d28cd
Nguyễn Việt Hưng <118376380+betothewizard@users.noreply.github.com>
2024-11-15 16:45:58 +0700

    🚀 first deployment

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import { Hono } from "hono";
 import { bearerAuth } from "hono/bearer-auth";
+import { logger } from "hono/logger";
 
 const app = new Hono();
 
 const token = "r4nd0mtok3n";
 
+app.use(logger());
 app.use("/api/*", bearerAuth({ token }));
 
 app.get("/api/page", (c) => {
